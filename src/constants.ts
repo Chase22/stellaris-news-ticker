1081d713d77841fd12561d50239839d6a93b8096
Chase <Chase@mailbox.org>
2025-03-23 21:14:49 +0100

    refactor: rewrite to svelte

---
+++ src/constants.ts
@@ -0,0 +1,20 @@
+export const empires = [
+    "Alarian Confederacy",
+    "Confederacy of Velutarian Systems",
+    "Imperial Union of Iryum",
+    "Kingdom of the Bay",
+    "Kressh Psi Lords",
+    "Olyrian Mandate",
+    "Order of the Toxic God",
+    "The Celestial Conclave",
+    "The Cruthian Research Facilities",
+    "The Dominion of the Machine Mother",
+    "The Niraveolan Ascendancy",
+    "The Shraak explorator empire",
+    "The shroomian Mycelia",
+    "The Trade Republic of Sanctourus",
+    "U'tai Fleet",
+    "Uthra'zai",
+]
+
+export const startDate = new Date("2200-1-1")
